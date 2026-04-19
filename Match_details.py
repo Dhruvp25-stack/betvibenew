bf0f5a391fe6d009a38d9240b663bbfcff28410f
Dhruvp25-stack <dhpatnu@gmail.com>
2026-04-19 21:12:41 +0530

    Update Match_details.py

--- Match_details.py
+++ Match_details.py
@@ -1,31 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-import os
-import time
-import json
-import pandas as pd
+from webdriver_manager.chrome import ChromeDriverManager
 
-# =====================================================
-# RENDER SELENIUM FIX
-# =====================================================
-chrome_options = Options()
+options = Options()
+options.binary_location = "/usr/bin/chromium"
 
-chrome_options.binary_location = "/usr/bin/chromium"
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
 
-chrome_options.add_argument("--headless=new")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--remote-debugging-port=9222")
-chrome_options.add_argument("--window-size=1920,1080")
-
-service = Service("/usr/bin/chromedriver")
+service = Service(ChromeDriverManager().install())
 
 driver = webdriver.Chrome(
     service=service,
-    options=chrome_options
+    options=options
 )
 
 # =====================================================
